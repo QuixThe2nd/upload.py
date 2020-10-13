f166b73e67e82d3b15f9bab8dae2f59ded3465a7
CrafterPika <48294025+CrafterPika@users.noreply.github.com>
2020-10-13 12:26:52 +0200

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='upload.py',
-    version='0.1.4',
+    version='0.15',
     description='An mutiple upload service library',
     url='https://github.com/CrafterPika/upload.py',
     author='upload.py',
